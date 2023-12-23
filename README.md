f5ea77d3c5b1b4b7f0adf23f79ab106e2c215a1e
Andi <largo@users.noreply.github.com>
2023-12-23 14:47:16 +0900

    Change name

---
+++ README.md
@@ -0,0 +1,7 @@
+# Browser Extension for Firefox. Be warned when there are too many windows
+
+I often end up with too many windows and want to switch through them quickly. This warns me when I have too many and lets me switch between them easily
+
+
+(*) Alpha version. I hacked this up in a few minutes.
+First version 2023/12/23
--- background.js
+++ background.js
@@ -1,16 +1,26 @@
-function updatePopup() {
-    browser.windows.getAll().then((windows) => {
-        console.log(windows.length);
-        browser.browserAction.setPopup({popup: "/popup.html"});
+function updateIcon(windowCount) {
+    let svgData = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" width="200" height="200">
+    <text x="50%" y=".9em" fill="hsl(36, 100%, 50%)" font-size="180" font-family="system-ui, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'" text-anchor="middle">📝</text>
+    <rect x="40" y="90" width="120" height="140" fill="rgba(240, 21, 21, 0.59)"/>
+    <text x="100" y="200" fill="#000" font-size="150" font-family="system-ui, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif" text-anchor="middle" id="windowCount">${windowCount}</text>
+  </svg>`;
+  
+    let encodedData = encodeURIComponent(svgData);
+    browser.browserAction.setIcon({ path: `data:image/svg+xml;charset=utf-8,${encodedData}` });
+}  
 
-        if (windows.length > 2) {
-        //Services.prompt.alert(window, 'title of alert', 'alert msg');
-        //alert('Hello, World!'); 
-        const title = "notificationTitle";
-        const content = "too many windows"
+function updatePopup() {
+  // Update icon on startup
+  checkAndUpdateIcon();
+
+    browser.windows.getAll().then((windows) => {
+        if (windows.length > 2) { 
+        const title = "Slow down!";
+        const content = "Too many windows"
         browser.notifications.create({
         type: "basic",
-        iconUrl: browser.extension.getURL("icons/link-48.png"),
+        iconUrl: browser.extension.getURL("smiley.svg"),
         title,
         message: content,
         });
@@ -29,4 +39,28 @@ function updatePopup() {
   
   // Listen for windows being closed
   browser.windows.onRemoved.addListener(updatePopup);
+  
+  function checkAndUpdateIcon() {
+    browser.windows.getAll().then((windows) => {
+      updateIcon(windows.length);
+    });
+  }
+
+//  browser.browserAction.setPopup({popup: "/popup.html"});
+
+browser.browserAction.setPopup({popup: ""});
+
+browser.browserAction.onClicked.addListener(() => {
+    browser.windows.getAll().then((windows) => {
+      if (windows.length > 1) {
+        // Assuming you want to switch to the next window in the list
+        let currentWindowIndex = windows.findIndex(win => win.focused);
+        let nextWindowIndex = (currentWindowIndex + 1) % windows.length;
+        let nextWindowId = windows[nextWindowIndex].id;
+  
+        // Switch to the next window
+        browser.windows.update(nextWindowId, { focused: true });
+      }
+    });
+  });
   
